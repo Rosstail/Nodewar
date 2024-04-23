7c7468cc8c86186c920d337b932a15a547d7834c
Rosstail <rosstail_zegeek@hotmail.fr>
2024-04-23 17:25:50 +0200

    Fixed Attack requirements not working properly

--- src/main/java/fr/rosstail/nodewar/territory/attackrequirements/AttackRequirements.java
+++ src/main/java/fr/rosstail/nodewar/territory/attackrequirements/AttackRequirements.java
@@ -50,48 +50,41 @@ public class AttackRequirements {
         if (startPoint) { //Cannot capture another startpoint if not targeted by other territory
             if (!ownedTerritoryList.isEmpty() &&
                     ownedTerritoryList.stream().noneMatch(territory1 -> territory1.getAttackRequirements().getTargetTerritoryList().contains(territory))) {
-                System.err.println("STARTPOINT BASE FAIL " + territory.getModel().getName() + " " + nwTeam.getModel().getName());
                 return false;
             }
         }
-
-        ownedTerritoryList.forEach(territory1 -> {
-            System.out.println(" > " + territory1.getModel().getName() + " is startpoint " + territory1.getAttackRequirements().startPoint);
-        });
 
         List<Territory> startPointList = ownedTerritoryList.stream().filter(
                 territory1 -> (territory1.getAttackRequirements().isStartPoint())
         ).collect(Collectors.toList());
 
         for (Territory startPoint : startPointList) {
-            System.out.println(" > " + startPoint.getModel().getName());
-            return checkAttackRequirements(nwTeam, startPoint, new ArrayList<>(), territory);
+            if(checkAttackRequirements(nwTeam, startPoint, new ArrayList<>(), territory)) {
+                return true;
+            }
         }
-        System.err.println("STARTPOINT DEFINITIVE FAIL " + territory.getModel().getName() + " " + nwTeam.getModel().getName());
-
         return false;
     }
 
     public boolean checkAttackRequirements(NwTeam nwTeam, Territory territoryToCheck, ArrayList<Territory> territoryToIgnoreList, Territory finalTerritory) {
-        System.out.println("checking " + territoryToCheck.getModel().getName() + " if is  " + finalTerritory.getModel().getName());
+        List<Territory> territoryListToCheck = territoryToCheck.getAttackRequirements().getTargetTerritoryList().stream()
+                .filter(territory1 -> (!territoryToIgnoreList.contains(territory1))).collect(Collectors.toList());
 
-        territoryToIgnoreList.add(territoryToCheck);
-        if (finalTerritory == territoryToCheck) {
+        if (territoryListToCheck.contains(finalTerritory)) {
             return true;
         }
 
-        List<Territory> territoryListToCheck = territoryToCheck.getAttackRequirements().getTargetTerritoryList().stream()
-                .filter(territory1 -> (
-                        !territoryToIgnoreList.contains(territory1) && territory1.getOwnerTeam() == nwTeam
-                )).collect(Collectors.toList());
+        if (territoryToCheck.getOwnerTeam() != nwTeam) {
+            return false;
+        }
+
+        territoryToIgnoreList.add(territoryToCheck);
 
         for (Territory territory1 : territoryListToCheck) {
             if (checkAttackRequirements(nwTeam, territory1, new ArrayList<>(territoryToIgnoreList), finalTerritory)) {
                 return true;
             }
         }
-        System.err.println("TO CHECK JAAJ FAIL " + territoryToCheck.getModel().getName() + " " + nwTeam.getModel().getName());
-
         return false;
     }
 
